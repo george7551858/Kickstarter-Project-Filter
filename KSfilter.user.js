30d3e809e6c9e9543f5d0fecda4c0e770aeaf080
GG2 <george7551858@gmail.com>
2015-04-15 23:59:19 +0800

    Add filter input.

--- KSfilter.user.js
+++ KSfilter.user.js
@@ -13,5 +13,19 @@
 // ==/UserScript==
 
 (function() {
-    $(".project-blurb:contains('iPhone')").closest("li.project").hide();
+   $('<input type="text" id="filterTxt"/>').appendTo("div.back-to-top");
+    var $filterBtn = $('<button id="filterBtn" type="button">Filter</button>');
+    $filterBtn.appendTo("div.back-to-top");
+     var $clearBtn = $('<button id="clearBtn" type="button">Clear</button>');
+     $clearBtn.appendTo("div.back-to-top");
+    $filterBtn.on("click",function(event){
+        event.preventDefault();
+        var keyword = $("#filterTxt").val();
+       $(".project-blurb:contains("+keyword+")").closest("li.project").hide("slow");        
+    });
+    $clearBtn.on("click",function(event){
+        event.preventDefault();
+       $("li.project").show();
+    });
+    
 })();
