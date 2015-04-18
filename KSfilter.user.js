9b9da7d667c0866f1cf15d6b0a4935f48fcc03fa
GG2 <george7551858@gmail.com>
2015-04-18 19:35:10 +0800

    Remove "Filter", "Clear" button. Just press Enter in filter text input.

--- KSfilter.user.js
+++ KSfilter.user.js
@@ -12,7 +12,6 @@
 // @require     http://code.jquery.com/jquery-2.1.3.min.js
 // ==/UserScript==
 
-
 jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function(arg) {
     return function( elem ) {
         return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
@@ -24,20 +23,25 @@ jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function(arg) {
         "position": "fixed",
         "right": 0,
         "bottom": 0
-    });
-    $('<input type="text" id="filterTxt" placeholder="Blocked String"/>').appendTo($filterDiv);
-    var $filterBtn = $('<button id="filterBtn" class="btn btn--border-gray btn--small" type="button">Filter</button>').appendTo($filterDiv);
-    var $clearBtn = $('<button id="clearBtn" class="btn btn--border-gray btn--small" type="button">Clear</button>').appendTo($filterDiv);
-    
-    $filterDiv.appendTo("body");
-    $filterBtn.on("click",function(event){
-        event.preventDefault();
-        var keyword = $("#filterTxt").val();
-        $(".project-blurb:icontains("+keyword+")").closest("li.project").hide("slow");        
-    });
-    $clearBtn.on("click",function(event){
-        event.preventDefault();
+    }).appendTo("body");
+    var $filterTxt = $('<input type="text" id="filterTxt" placeholder="Filter String"/>').appendTo($filterDiv);
+
+    var doFilter =  function(){
         $("li.project").show();
+        var keyword = jQuery.trim($("#filterTxt").val());
+        console.log("keyword",keyword);
+        if( keyword === "" ) {
+            $filterTxt.css("border-color", "#d9d9de");
+            return;
+        }
+        else {
+            $filterTxt.css("border-color", "#2bde73");
+            $(".project-blurb:icontains("+keyword+")").closest("li.project").hide("slow");
+        }
+    };
+
+    $filterTxt.on("keypress",function(event){
+        if ( event.which == 13 ) doFilter();
     });
 
 })();
